7a64eba54f64dcc4abe97451fdeb5268389f302b
isaiah973 <adeolaisaiah01@gmail.com>
2026-03-27 22:07:11 +0100

    new portfolio

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -163,7 +163,7 @@ export default function Navbar() {
                 </button>
               );
             })}
-
+            {/* Hire button */}
             <button
               onClick={() => scrollToSection("contact")}
               className="mt-2 bg-teal-600 text-white px-4 py-2.5 rounded-full text-sm cursor-pointer"
